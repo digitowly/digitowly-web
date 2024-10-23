5f8990b601800542a9ad6ff2e9261a3e665ac0f2
ropfoo <ropfoo@gmail.com>
2024-10-23 19:53:42 +0200

    feat: add color config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,12 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    colors: {
+      green: "#37DD6E",
+      lightblue: "#37D2DD",
+      lightgray: "#F9F9F9",
+      white: "#fff",
+    },
   },
   plugins: [],
 };
