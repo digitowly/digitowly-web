3b5021423134af62f3ba5c194ddfb5f7f3be2f5f
ropfoo <ropfoo@gmail.com>
2024-10-27 22:55:28 +0100

    feat: add card setup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,9 @@ export default {
     colors: {
       green: "#37DD6E",
       lightblue: "#37D2DD",
+      iceblue: "#1B4A5D",
       lightgray: "#F9F9F9",
+      dark: "#0d101f",
       white: "#fff",
     },
   },
