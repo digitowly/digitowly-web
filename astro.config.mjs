e5e49e9f46044a725034e0767174a7d52c0050fa
ropfoo <ropfoo@gmail.com>
2025-07-02 21:35:00 +0200

    feat: update astro and remove solid-js dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import solidJs from "@astrojs/solid-js";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()],
+  integrations: [tailwind()],
 });
